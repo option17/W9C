460db600423f828d676a4da93d8af128658e0fa5
Your Name <you@example.com>
2022-07-08 23:25:03 -0600

    Finished assignment

---
+++ app.js
@@ -0,0 +1,26 @@
+let itemNames = [`dogs`,`cats`,`birds`];
+let itemPrices = [30, 20, 0]
+let inStock = [true, false, true,];
+
+let firstItemName = itemNames[0];
+console.log(firstItemName);
+
+let firstItemPrice = itemPrices[0];
+console.log(firstItemPrice);
+
+itemNames.push(`snakes`);
+itemPrices.push(100);
+inStock.push(false);
+
+lastItemName = itemNames.pop(3)
+lastItemPrice = itemPrices.pop(3)
+lastItemStock = inStock.pop(3)
+
+
+
+
+
+
+
+
+ 
